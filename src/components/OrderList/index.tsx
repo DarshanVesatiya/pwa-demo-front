9f1ad923457f4a3ed160a7c02379d677a9de3455
DarshanVesatiya <darshan.vesatiya@rapidops.com>
2022-01-05 12:45:44 +0530

    Added loader for order list.

--- src/components/OrderList/index.tsx
+++ src/components/OrderList/index.tsx
@@ -1,10 +1,10 @@
-import { listeners } from "process";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const OrderList = (): JSX.Element => {
   const [orderList, setOrderList] = useState([]);
-  const { userId } : any = useParams();
+  const { userId }: any = useParams();
+  const [isFetchingOn, setFetchingOn] = useState(false);
 
   useEffect(() => {
     fetchOrderList()
@@ -12,21 +12,21 @@ const OrderList = (): JSX.Element => {
 
   const getStatusComp = (status: any) => {
     let component;
-    switch(status) {
-      case "Accepted": 
-        component = <span style={{color: 'greenyellow'}}>{status}</span>
+    switch (status) {
+      case "Accepted":
+        component = <span style={{ color: 'greenyellow' }}>{status}</span>
         break;
-      case "Preparing": 
-        component = <span style={{color: 'blue'}}>{status}</span>
+      case "Preparing":
+        component = <span style={{ color: 'blue' }}>{status}</span>
         break;
-      case "ReadyToDeliver": 
-        component = <span style={{color: 'orange'}}>{status}</span>
+      case "ReadyToDeliver":
+        component = <span style={{ color: 'orange' }}>{status}</span>
         break;
-      case "Delivered": 
-        component = <span style={{color: 'green'}}>{status}</span>
+      case "Delivered":
+        component = <span style={{ color: 'green' }}>{status}</span>
         break;
-      case "Cancelled": 
-        component = <span style={{color: 'red'}}>{status}</span>
+      case "Cancelled":
+        component = <span style={{ color: 'red' }}>{status}</span>
         break;
       default:
         component = <span>{status}</span>
@@ -36,58 +36,66 @@ const OrderList = (): JSX.Element => {
 
   const fetchOrderList = () => {
     try {
+      setFetchingOn(true);
       fetch(`${process.env.REACT_APP_API_ENDPOINT}v1/user/${userId}/order-list`)
         .then((response) => response.json())
         .then((...data: any) => {
           setOrderList(data[0].Data);
+          setFetchingOn(false);
         })
-        .catch();
+        .catch(() => {
+          setFetchingOn(false);
+        });
     } catch (error) {
-      
+      setFetchingOn(false);
     }
   }
 
   return (
     <div className="container">
       <div className="card">
-        <div className="table-responsive orderListItems ">
-      <table className="table table-hover responsiveTable">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Order Id</th>
-            <th scope="col">Order Date</th>
-            <th scope="col">Status</th>
-            <th scope="col">Total amount</th>
-            <th scope="col">Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            orderList.length
-              ? (
-                <>
+        {isFetchingOn ? (
+          <div className="loaderBox"><div className="loader"></div></div>
+        ) : (
+          <div className="table-responsive orderListItems ">
+            <table className="table table-hover responsiveTable">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Order Id</th>
+                  <th scope="col">Order Date</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Total amount</th>
+                  <th scope="col">Location</th>
+                </tr>
+              </thead>
+              <tbody>
                 {
-                  orderList.map((order: any, index) => (
-                    <tr>
-                      <th scope="row">{index + 1}</th>
-                      <td>{order._id}</td>
-                      <td>{new Date(order.date).toLocaleString()}</td>
-                      <td>{getStatusComp(order.status)}</td>
-                      <td>{order.totalAmount}</td>
-                      <td>{order.address}</td>
-                    </tr>
-                  ))
+                  orderList.length
+                    ? (
+                      <>
+                        {
+                          orderList.map((order: any, index) => (
+                            <tr>
+                              <th scope="row">{index + 1}</th>
+                              <td>{order._id}</td>
+                              <td>{new Date(order.date).toLocaleString()}</td>
+                              <td>{getStatusComp(order.status)}</td>
+                              <td>{order.totalAmount}</td>
+                              <td>{order.address}</td>
+                            </tr>
+                          ))
+                        }
+                      </>
+                    )
+                    : (
+                      <><tr><td className="p-5 text-center" colSpan={6}>No Data Found</td></tr></>
+                    )
                 }
-                </>
-              )
-              : (
-                <><tr><td className="p-5 text-center" colSpan={6}>No Data Found</td></tr></>
-              )
-          }
-        </tbody>
-      </table>
-      </div>
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </div>
   );
